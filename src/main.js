560719a459a62b8b17c63d74fb1234c54e90a3a6
Celia Velasco <24230339+celiavelmar@users.noreply.github.com>
2022-09-10 17:16:33 +0200

    feat: Add Tailwind and Flowbite

--- src/main.js
+++ src/main.js
@@ -1,8 +1,11 @@
 import { createApp } from 'vue';
 import { createPinia } from 'pinia';
+import 'flowbite';
 
 import App from './App.vue';
 import router from './router';
+import './assets/css/base.css';
+import './assets/css/index.css';
 
 const app = createApp(App);
 
