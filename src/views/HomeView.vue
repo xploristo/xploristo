09795ef052e0e093163ecdd6c1fbe3d1f587bcee
Celia Velasco <24230339+celiavelmar@users.noreply.github.com>
2022-09-10 17:13:36 +0200

    feat: Remove unneeded example components

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,9 +1,11 @@
-<script setup>
-import TheWelcome from '@/components/TheWelcome.vue';
-</script>
-
 <template>
   <main>
-    <TheWelcome />
+    <p class="">Hello</p>
   </main>
 </template>
+
+<script>
+export default {
+  name: 'HomeView',
+};
+</script>
