79b89922947cd9934430e526d427c3e5f9cf5f27
Celia Velasco <24230339+celiavelmar@users.noreply.github.com>
2022-12-06 16:44:59 +0100

    feat: Make confirm delete button blue instead of red

--- src/components/modals/ConfirmModal.vue
+++ src/components/modals/ConfirmModal.vue
@@ -20,7 +20,7 @@
           <button
             data-modal-toggle="popup-modal"
             type="button"
-            class="text-white bg-red-500 hover:bg-red-600 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
+            class="text-white bg-blue-500 hover:bg-blue-600 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm inline-flex items-center px-5 py-2.5 text-center mr-2"
             @click="$emit('confirm')"
           >
             {{ $t(confirm) }}
